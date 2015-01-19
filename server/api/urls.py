27ae3ea43b8fe2b677bbf4d5ec56b42210631da8
Jeremy T <jeremy@getnomi.com>
2015-01-18 21:48:48 -0500

    ViewSet and Router simplifies API even more

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import patterns, url, include
+from rest_framework.routers import DefaultRouter
 
 from api import views
 
-urlpatterns = format_suffix_patterns(
-    patterns('',
-        url(r'^$', views.api_root, name='api-root'),
-        url(r'^players/$', views.PlayerList.as_view(), name='player-list'),
-        url(r'^players/(?P<pk>[0-9]+)/$', views.PlayerDetail.as_view(), name='player-detail')
-    )
+router = DefaultRouter()
+router.register('players', views.PlayerViewSet)
+
+urlpatterns = patterns('',
+    url(r'^', include(router.urls)),
 )
--- server/api/views.py
+++ server/api/views.py
@@ -1,25 +1,11 @@
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from rest_framework import viewsets
 
 from common.models import Player
 from api.serializers import PlayerSerializer
 
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'players': reverse('player-list', request=request, format=format)
-    })
 
-class PlayerList(generics.ListCreateAPIView):
-    queryset = Player.objects.all()
-    serializer_class = PlayerSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-
-class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
     permission_classes = (permissions.IsAuthenticated,)
