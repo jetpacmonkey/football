9791068f35ff3a9c5088068d25536e715dfcb2f2
Jeremy T <jeremy@getnomi.com>
2015-02-01 17:27:16 -0500

    Fleshing out draft urlconf

--- server/draft/urls.py
+++ server/draft/urls.py
@@ -4,4 +4,6 @@ from draft import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^create$', views.create, name='create'),
+    url(r'^(?P<draftId>\d+)$', views.singleDraft, name='singleDraft'),
 )
--- server/draft/views.py
+++ server/draft/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from draft.models import Draft
 
@@ -9,9 +9,15 @@ def index(request):
     })
 
 
-# Create your views here.
 def create(request):
     return render(request, 'draft/create.html', {
         'PLAYER_TYPE_CHOICES': Draft.PLAYER_TYPE_CHOICES,
         'DEFAULT_PLAYER_TYPE': Draft._meta.get_field_by_name('type')[0].default
+    })
+
+
+def singleDraft(request, draftId):
+    draft = get_object_or_404(Draft, id=draftId)
+    return render(request, 'draft/singleDraft.html', {
+
     })
