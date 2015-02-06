3aa3798cb9a7c571be01dfe21306069850b4c7f7
Jeremy T <jeremy@getnomi.com>
2015-02-05 23:30:16 -0500

    adding some help text for Player model

--- server/common/models/player.py
+++ server/common/models/player.py
@@ -8,21 +8,21 @@ class Player(Base):
     last_name = models.CharField(max_length=64)
 
     # offensive attributes
-    passing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(3)])
-    hands = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(20)])
-    rushing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    receive_short = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    receive_mid = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    receive_deep = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    blocking = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    kicking = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
+    passing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(3)], help_text="0-3")
+    hands = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(20)], help_text="0-20")
+    rushing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    receive_short = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    receive_mid = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    receive_deep = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    blocking = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    kicking = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
 
     # offensive_special = models.ManyToManyField(SpecialAbility, limit_choices_to={'type': 'offense'})
 
-    pass_man = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(3)])
-    pass_zone = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    run_defense = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
-    blitzing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)])
+    pass_man = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(3)], help_text="0-3")
+    pass_zone = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    run_defense = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
+    blitzing = models.PositiveSmallIntegerField(validators=[validators.MaxValueValidator(5)], help_text="0-5")
 
     # defensive_special = models.ManyToManyField(SpecialAbility, limit_choices_to={'type': 'defense'})
 
